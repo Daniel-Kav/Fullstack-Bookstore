dea573714a42688d495beab70289a451b095289f
daniel-kav <dankavatha01@gmail.com>
2024-06-27 09:49:58 +0300

    config

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import BookRepository from './components/BookRepository';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <BookRepository />
+    </div>
+  );
+}
+
+export default App;
